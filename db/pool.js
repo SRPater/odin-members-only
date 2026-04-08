b0111458271078fe4dd22196eb992240ca2c90af
SRPater <srpater@gmail.com>
2026-04-08 12:24:26 +0200

    Add SSL to Pool for Neon

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,6 @@
 import { Pool } from 'pg';
 
 export default new Pool({
-   connectionString: process.env.DATABASE_URL
+   connectionString: process.env.DATABASE_URL,
+   ssl: { rejectUnauthorized: false },
 });
